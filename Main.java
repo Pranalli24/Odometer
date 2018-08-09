6c1e3eb74b053335f6fffa22f1ddd9fec8633063
Pranalli24 <pranallic24@gmail.com>
2018-08-09 13:33:17 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+
+public class Main {
+
+	public static void main(String []args)
+	{
+		Odometer o= new Odometer(4);
+		System.out.println(o.nextNum(num));
+		System.out.println(o.prevNum(num));
+		System.out.println(o.incrementN(4, num));
+		System.out.println(o.decrementN(4, num));
+		System.out.println(o.difference(num1, num2));
+		
+		
+		
+		
+		
+	}
+	
+	
+}
